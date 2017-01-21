baae95fe568e35496027be7e501a963acca932c9
peter.fry <pfry@redhat.com>
2017-01-21 21:30:25 +0000

    adding sample java unit test using selenium

---
+++ java-sample-selenium-grid/src/test/java/org/pfry/selenium/SeleniumTest.java
@@ -0,0 +1,44 @@
+package org.pfry.selenium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumTest {
+	
+	@Test
+	public void chromeTest() throws MalformedURLException{
+		String hubURL = "http://selenium-hub-selenium.rhel-cdk.10.1.2.2.xip.io/wd/hub";
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL(hubURL), capability);
+		driver.get("http://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Cheese!");
+		element.submit();
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+        
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cheese!");
+            }
+        });
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + driver.getTitle());
+        
+        //Close the browser
+        driver.quit(); 
+	}
+
+}
