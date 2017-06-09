379dd641c9c1dcf099b0cb4497a3427662a669a0
Peter Fry <peterfry1900@gmail.com>
2017-06-09 20:06:30 +0100

    Create entry_point.sh

--- Hub/entry_point.sh
+++ Hub/entry_point.sh
@@ -19,11 +19,11 @@ function shutdown {
     echo "shutdown complete"
 }
 
-java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
-  -role hub \
-  -hubConfig $CONF \
-  ${SE_OPTS} &
-NODE_PID=$!
+#java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+#  -role hub \
+#  -hubConfig $CONF \
+#  ${SE_OPTS} &
+#NODE_PID=$!
 
 trap shutdown SIGTERM SIGINT
 wait $NODE_PID
